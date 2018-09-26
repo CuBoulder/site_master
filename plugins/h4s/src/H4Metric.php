651cd521e536742a8af3d3825b714cdbb3150a1f
Alex Finnarn <alexfinnarn@gmail.com>
2018-09-26 14:20:06 -0700

    start to h4 metric plugin

---
+++ plugins/h4s/src/H4Metric.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @file
+ * Contains H4Metric class.
+ */
+
+namespace SiteMaster\Plugins\H4Metric;
+
+use SiteMaster\Core\Auditor\MetricInterface;
+use SiteMaster\Core\Auditor\Logger;
+use SiteMaster\Core\Auditor\Site\Page;
+
+class H4Metric extends MetricInterface
+{
+
+    /**
+     * @param string $plugin_name
+     * @param array $options
+     */
+//    public function __construct($plugin_name, array $options = array())
+//    {
+//        parent::__construct($plugin_name, $options);
+//    }
+
+    /**
+     * Get the human readable name of this metric
+     *
+     * @return string The human readable name of the metric
+     */
+    public function getName()
+    {
+        return 'H4 Check';
+    }
+
+    /**
+     * Get the Machine name of this metric
+     *
+     * This is what defines this metric in the database
+     *
+     * @return string The unique string name of this metric
+     */
+    public function getMachineName()
+    {
+        return 'h4_metric';
+    }
+
+    /**
+     * Determine if this metric should be graded as pass-fail
+     *
+     * @return bool True if pass-fail, False if normally graded
+     */
+    public function isPassFail()
+    {
+        return true;
+    }
+
+    /**
+     * Scan a given URI and apply all marks to it.
+     *
+     * All that this
+     *
+     * @param string $uri - the uri to scan
+     * @param \DOMXPath $xpath - the xpath of the uri
+     * @param int $depth - the current depth of the scan
+     * @param \SiteMaster\Core\Auditor\Site\Page $page - the current page to scan
+     * @param Logger\Metrics $logger The logger class which calls this method, you can access the spider, page, and scan from this
+     *
+     * @return bool True if there was a successful scan, false if not.  If false, the metric will be graded as incomplete
+     */
+    public function scan($uri, \DOMXPath $xpath, $depth, Page $page, Logger\Metrics $logger)
+    {
+
+    }
+}
