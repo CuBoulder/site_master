dbf7b08345c85ba320d0848df8bb936f2e3fc7a2
Alex Finnarn <alexfinnarn@gmail.com>
2018-10-15 15:04:28 -0700

    show debugging of method that exists but can't be found

--- plugins/foo/www/templates/html/Listing.tpl.php
+++ plugins/foo/www/templates/html/Listing.tpl.php
@@ -1,12 +1,17 @@
-<?php
-
-//print count($data);
-?>
 
 <h2>Foo</h2>
 
 <pre>
- <?php print_r($data); ?>
+ <?php
+   $class_methods = get_class_methods($data);
+
+   foreach ($class_methods as $method_name) {
+     echo "$method_name\n";
+   }
+
+   print_r($data->current());
+
+ ?>
 </pre>
 
 
